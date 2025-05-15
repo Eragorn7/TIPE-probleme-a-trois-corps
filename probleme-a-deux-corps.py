25fa54ff497c1d5e551c3025c1f8cbe05b680317
Eragorn <antoinezhang07@gmail.com>
2025-05-15 12:44:46 +0200

    Update probleme-a-deux-corps.py

--- probleme-a-deux-corps.py
+++ probleme-a-deux-corps.py
@@ -1,2 +1,7 @@
 """Modélisation Python du mouvement d'une planète dans le champs gravitationnel d'une étoile"""
 
+# coordonnées polaires
+
+
+mu = 0.001 #masse relative de la planète par rapport à l'étoile
+
