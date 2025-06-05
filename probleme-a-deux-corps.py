55059a32416eecdfc58afcda4c7ced0dee501f33
Eragorn <antoinezhang07@gmail.com>
2025-06-05 12:54:36 +0200

    Update probleme-a-deux-corps.py

--- probleme-a-deux-corps.py
+++ probleme-a-deux-corps.py
@@ -1,7 +1,25 @@
 """Modélisation Python du mouvement d'une planète dans le champs gravitationnel d'une étoile"""
 
+from math import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 # coordonnées polaires
 
 
 mu = 0.001 #masse relative de la planète par rapport à l'étoile
 
+v0 = 3351 #km/mois    vitesse approximative de la Terre
+R = 6400
+omega0 = v0 / R
+x0, y0 = R/sqrt(2) + R, R/sqrt(2) + R/2
+
+
+Lt = np.array([i for i in range(14)])
+Lx = R * cos(omega0 * Lt) + x0 
+Ly = R * sin(omega0 * Lt) + y0
+
+plt.plot(Ly, Lx, '+')
+plt.legend("distances en km")
+plt.title("Mouvement d'une planète en rotation autour d'une étoile")
