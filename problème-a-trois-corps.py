e4240f79fc7128fa61556b77ba4db5e8534b9ced
Eragorn <antoinezhang07@gmail.com>
2025-11-10 15:04:32 +0100

    Initialize variables and setup imports for simulation
    Added imports and initialized variables for simulation.

--- problème-a-trois-corps.py
+++ problème-a-trois-corps.py
@@ -1,8 +1,24 @@
 # importation des modules 
 from scipy.integrate import odeint
+import maths
+import matplotlib.pyplot as plt
 
 # initialisation des variables globales
+x0, y0 = 0.5*1.5e8, 0
+vx0, vy0 = 0,0
+init = [x0, vx0, y0, vy0]
+G = 6.7e-11
+m_1 = 2e30
+m_2 = 6e24
+m_3 = 6.5e3
+omega = sqrt(G * (m_1 + m_2) / d**3)
+r_1 = -m_2/(m_2 + m_1)
+r_2 = m_1/(m_1 + m_2)
 
+tmax = 10
+N = 50
+dt = tmax/N
+temps = np.linspace(0, tmax, N)
 
 
 # equation du mouvement 
@@ -16,11 +32,17 @@ def eq(x,y,vx,vy):
 
 
 
-# representation graphique
-def trajectoire(tmax) :
+#obtention de la trajectoire dans le referentiel tournant
+def trajectoire() :
 
 
 
   traj = odeint(eq, init, temps)
   liste_x = traj[1][0]
   liste_y = traj[1][2]
+  plt.plot(-r_1,0,"yo")
+  plt.plot(r_2,0,"yo")
+  plt.plot(liste_x,liste_y,"b+")
+  
+
+# representation graphique
